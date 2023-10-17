51d8924d988d75c295ae8546101a7e3e6d58fa2a
Ogheneyoma <Lawrenceyoma@gmail.com>
2023-10-18 00:11:40 +0100

    feat: created signup api route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config(); // allows us use DOT env files
 
 
-const app = express();;
+const app = express();
+
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB');
@@ -15,3 +19,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
     console.log(err);
 })
+
+app.use('/api/user', userRoutes)
+
+app.use('/api/auth', authRoutes)
