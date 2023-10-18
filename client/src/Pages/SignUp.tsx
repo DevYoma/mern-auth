e2aabc80b69a24d6804eb4f5b07ae4fbfd6e0f0b
Ogheneyoma <Lawrenceyoma@gmail.com>
2023-10-18 02:07:47 +0100

    feat: user sign up complete

--- client/src/Pages/SignUp.tsx
+++ client/src/Pages/SignUp.tsx
@@ -1,37 +1,95 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
+
 const SignUp = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [formData, setFormData] = useState<any>({
+    username: '', 
+    email: '', 
+    password: ''
+  })
+
+  const handleChange = (e: any) => {
+    setFormData({ 
+      ...formData, 
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // console.log(formData);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    // console.log(formData);
+
+    try {
+      setLoading(true)
+      setError(false); 
+      const response: Response = await fetch('/api/auth/signup', {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(formData)
+      });
+  
+      const data = await response.json();
+      console.log(data);
+      setLoading(false);
+
+      if(data?.success === false){
+        setError(true);
+        return
+      }
+    } catch (error) {
+      setLoading(false)
+      setError(true)
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
 
       <form 
         className="flex flex-col gap-4"
+        onSubmit={handleSubmit}
       >
         <input 
           type="text" 
           placeholder="Username"
-          id="username"
+          name="username"
           className="bg-slate-100 p-3 rounded-lg"
+          value={formData.username}
+          onChange={handleChange}
+          required
         />
         <input 
           type="email" 
           placeholder="Email"
-          id="email"
+          name="email"
           className="bg-slate-100 p-3 rounded-lg"
+          value={formData.email}
+          onChange={handleChange}
+          required
         />
         <input 
           type="password" 
-          placeholder="Passowrd"
-          id="password"
+          placeholder="Password"
+          name="password"
           className="bg-slate-100 p-3 rounded-lg"
+          value={formData.password}
+          onChange={handleChange}
+          required
         />
 
         <button 
-          disabled
-          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 disabled:cursor-not-allowed"
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
         >
-          Sign Up
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
 
@@ -41,6 +99,7 @@ const SignUp = () => {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong"}</p>
     </div>
   )
 }
