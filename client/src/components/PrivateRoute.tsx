ad08db573e6a144871c052e70fc60393597243e3
Ogheneyoma <Lawrenceyoma@gmail.com>
2023-10-27 12:14:34 +0100

    feat: added private route and updated header component

---
+++ client/src/components/PrivateRoute.tsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux"
+import { Outlet, Navigate } from "react-router-dom"
+
+const PrivateRoute = () => {
+    // @ts-ignore
+    const { currentUser } = useSelector(state => state.user)
+  return currentUser ? <Outlet /> : <Navigate to={'/sign-in'}/>
+}
+
+export default PrivateRoute
